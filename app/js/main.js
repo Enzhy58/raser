12f84b33b8f8edfabfeb64cb831eb91c74c109b6
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-17 11:22:10 +0300

    added gear section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $('.news__list').slick({
+    $('.news__list, .gear__list').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icon/arrow-next.svg" alt=""></button>'
