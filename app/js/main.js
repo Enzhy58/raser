f114681a5cf737fd238b445195227cf2b9ae1bb2
Enzhy58 <ang.borisova2015@yandex.ru>
2024-03-11 17:01:29 +0300

    added exclusive section

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,21 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icon/arrow-next.svg" alt=""></button>'
     });
-  
-  
+    $('.exclusive__list').slick({
+        slidesToShow: 4,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icon/arrow-next.svg" alt=""></button>'
+    });
+
+    $(".star__img").rateYo({
+        maxValue: 1,
+        numStars: 1,
+        starWidth: "18px",
+        starHeight: "17px",
+        ratedFill: "#F2C94C"
+    });
+
+
+
 });
